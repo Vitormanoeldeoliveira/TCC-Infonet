287af8f7eacf540efa51578857a8bb27ec699c99
vitor oliveira <vitor.oliveira@go4digital.com.br>
2023-08-04 15:39:48 -0300

    ...

--- src/modules/user/complements/token/model/token.model.ts
+++ src/modules/user/complements/token/model/token.model.ts
@@ -1,5 +1,5 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { IToken } from '../Interface/token.interface';
+import { IToken } from '../interface/token.interface';
 
 @ObjectType({ implements: IToken })
 export class Token extends IToken {}
